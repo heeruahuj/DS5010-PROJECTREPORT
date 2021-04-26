c91a1cbee633455f0d1626caabfe3327c130b28b
Tanmay Khokle <tanmaykhokle@gmail.com>
2021-04-25 23:05:08 -0400

    removed unnecessary import

--- exampleNB.py
+++ exampleNB.py
@@ -4,7 +4,6 @@ from NaiveBayes import NaiveBayesClassifier
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn import datasets
-import matplotlib.pyplot as plt
 
 #Calculating accuracy --(actual-prediction)/(total no of samples)
 def accuracy(y_actual, yhat):
